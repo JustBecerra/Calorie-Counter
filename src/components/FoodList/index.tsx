58c1b7153c5cd697f3a8fa161d7c40ed9334cc70
JustBecerra <justj.becerra@gmail.com>
2023-05-27 12:29:26 -0300

    gave list items container

--- src/components/FoodList/index.tsx
+++ src/components/FoodList/index.tsx
@@ -1,19 +1,42 @@
-import { Box, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { useFoodContext } from "../../context/FoodProvider";
 
 export const FoodList = () => {
-  const { filteredData, clearInput, setClearInput } = useFoodContext();
+  const { filteredData, clearInput } = useFoodContext();
   if(clearInput){
     filteredData.length = 0
   } else {
-    filteredData.length = 15
+    filteredData.length = 25
   }
+  console.log(filteredData)
   
   return (
-    <Box>
-      {filteredData.map((item) => 
-        <Typography>{item.Display_Name}</Typography>
+    <Stack sx={{
+      marginTop: '10vh',
+      flexDirection: 'row',
+      overflowY: 'scroll',
+      width: '50vw',
+      height: '70vh',
+      flexWrap: "wrap",
+      gridTemplateColumns: 3
+    }}>
+      {filteredData.map((item, key) => 
+        <Stack key={key} sx={{
+          marginY: '1rem',
+          padding: '0.5rem',
+          gap: '0.5rem',
+          alignItems: 'flex-start',
+          border: '1px solid green',
+          width: '80%',
+          height: '7rem'
+        }}>
+          <Typography sx={{alignSelf:'center'}}>{item.Display_Name}</Typography>
+          <Stack sx={{flexDirection:'row', marginTop:'0.5rem', gap:'22rem'}}>
+            <Typography maxWidth={'fit-content'}>{'Portion: ' + item.Portion_Amount + ' ' + item.Portion_Display_Name}</Typography>
+            <Typography maxWidth={'fit-content'}>{item.Calories + ' Calories per portion'}</Typography>
+          </Stack>
+        </Stack>
       )}
-    </Box>
+    </Stack>
   );
 };
